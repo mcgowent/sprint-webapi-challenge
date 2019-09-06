e0acb6ed5a851d69779a592988234ad27a4e4907
Timothy McGowen <mcgowen17@yahoo.com>
2019-09-06 09:48:49 -0700

    Able to get actions based off of the id and delete items based on id

--- _projects_/projectsRoute.js
+++ _projects_/projectsRoute.js
@@ -34,52 +34,53 @@ router.get('/', (req, res) => {
     projectsDB.get()
         .then(users => res.status(200).json(users))
         .catch(err => {
-            console.log('Error inside of get users')
-            res.status(500).json({ error: "The users info could not be retrieved." })
+            console.log('Error inside of get Projects')
+            res.status(500).json({ error: "The Project info could not be retrieved." })
         })
 });
 
-// router.get('/:id', (req, res) => {
-//     const { id } = req.params
-//     projectsDB.get(id)
-//         .then(users => {
-//             console.log(users.name)
-//             if (users.name) {
-//                 return res.status(200).json(users)
-//             }
-//         })
-//         .catch(err => {
-//             res.status(404).json({ error: "The User with that ID doesn't exsist" })
-//         })
-// });
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    console.log(id)
+    projectsDB.get(id)
+        .then(users => {
+            console.log(users.name)
+            if (users.name) {
+                return res.status(200).json(users)
+            }
+        })
+        .catch(err => {
+            res.status(404).json({ error: "The Project with that ID doesn't exsist" })
+        })
+});
 
-// router.get('/:id/posts', validatePost, validateUserId, (req, res) => {
-//     const { id } = req.params
-//     postDB.getById(id)
-//         .then(post => {
-//             console.log(post.user_id)
-//             res.status(200).json(post)
-//         })
-//         .catch(err => {
-//             res.status(404).json({ error: "The User with that ID doesn't exsist" })
-//         })
-// });
+router.get('/:id/actions', (req, res) => {
+    const { id } = req.params
+    projectsDB.getProjectActions(id)
+        .then(actions => {
+            console.log(actions)
+            res.status(200).json(actions)
+        })
+        .catch(err => {
+            res.status(404).json({ error: "The User with that ID doesn't exsist" })
+        })
+});
 
-// router.delete('/:id', validateUserId, (req, res) => {
-//     const { id } = req.params
-//     db.remove(id)
-//         .then(removed => {
-//             if (removed) {
-//                 res.status(204).end()
-//             } else (
-//                 res.status(404).json({ message: "The User with the specified ID does not exist." })
-//             )
-//         })
-//         .catch(err => {
-//             console.log(err)
-//             res.status(500).json({ error: "The User could not be removed." })
-//         })
-// });
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    projectsDB.remove(id)
+        .then(removed => {
+            if (removed) {
+                res.status(204).end()
+            } else (
+                res.status(404).json({ message: "The Project with the specified ID does not exist." })
+            )
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: "The Project could not be removed." })
+        })
+});
 
 // router.put('/:id', validateUserId, validateUser, (req, res) => {
 //     const { id } = req.params
