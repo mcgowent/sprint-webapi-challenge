7063f82a01556a4219a0afe6bd0029db231cc761
Timothy McGowen <mcgowen17@yahoo.com>
2019-09-06 09:35:54 -0700

    Able to post to projects endpoints

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// code away!
+// // code away!
 require('dotenv').config() // Allows use of a .env file to save the port
 
 const express = require('express') //importing express
@@ -7,8 +7,12 @@ const projectsRouter = require('./_projects_/projectsRoute')  // Connecting the 
 
 const port = process.env.PORT  // Use a project white variable to set the port
 
+
 const server = express() //setting up express
-server.use(express.json()) // Makes all data to be read as json
+// server.use(express.json()) // Makes all data to be read as json
+
+server.use(express.json())
+
 
 server.use('/api/actions', actionsRouter)  // Setting up initial endpoints
 server.use('/api/projects', projectsRouter)
